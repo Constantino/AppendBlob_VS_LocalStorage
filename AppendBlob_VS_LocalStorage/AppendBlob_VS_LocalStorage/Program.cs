0c6d95e070618fd7123230439b6e978bfbc47488
Constantino Mora <constantinomora@gmail.com>
2017-07-23 21:52:40 -0500

    Let's the code begin

---
+++ AppendBlob_VS_LocalStorage/AppendBlob_VS_LocalStorage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendBlob_VS_LocalStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
